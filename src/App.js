f666113d097bc9fe1c883db208628ed35667a9d6
jungwoni4080 <doge4080@gmail.com>
2021-06-07 21:41:03 +0900

    reactuuid설치전&버튼클릭시 작업작업중

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 
 import Sidebar from "./Sidebar"
 import Main from "./Main"
+import {useState} from "react";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote= () => {
+    const newNote = {
+      id: 
+      title:
+      body:
+      lastModified:
+    }
+  }
+
   return (
     <div className="App">
-      <Sidebar  />
+      <Sidebar notes = {notes} onAddNote={onAddNote} />
       <Main />
     </div>
   );
